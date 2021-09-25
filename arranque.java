e7907c247a2d62cac3a51aea0f814259a804c3cd
elpepeyetezetch <75713482+elpepeyetezetch@users.noreply.github.com>
2021-09-24 23:28:17 -0500

    modelo 1 de interfaz

---
+++ arranque.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.simulador_bancario;
+
+
+public class arranque {
+    
+   public static void main(String[] args){
+   
+       
+       
+     presentacion prin=new presentacion();
+    
+    prin.setVisible(true);
+    dispose();
+    
+    
+    
+    
+    }
+
+    private static void dispose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
